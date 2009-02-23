3e137f4acf2516803131ee79d69dadeb37f1dd41
boris.beaulant <boris.beaulant@76d89e8a-b44c-0410-a8b7-850d08addc1f>
2009-02-23 09:25:04 +0000

    - Bug fix : avoid the "Ok" that appear in Alert when there's no Yes button - Add A KuixCanvasInteractionListener class to be able to intercept UI events at low level. (@see Kuix.getCanvas().setInteractionListener(...))

--- trunk/src/org/kalmeo/kuix/core/KuixConstants.java
+++ trunk/src/org/kalmeo/kuix/core/KuixConstants.java
@@ -29,7 +29,7 @@ public interface KuixConstants {
 	/////////////////////////////////////////////////////////////////////////////////////////
 	// Commons
 	
-	public static final String VERSION										= "1.1.0";
+	public static final String VERSION										= "1.1.1";
 	
 	public static final String DEFAULT_XML_RES_FOLDER						= "/xml/";
 	public static final String DEFAULT_CSS_RES_FOLDER						= "/css/";
